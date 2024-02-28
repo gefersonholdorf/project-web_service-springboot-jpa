53998f5f0430731db8a69d92a1ee87d4e8cfdabf
Geferson Holdorf <gefersonholdorf88@gmail.com>
2024-02-28 20:25:34 -0300

    Project created

---
+++ src/main/java/com/gefersonholdorf/projectweb_servicespringbootjpa/ProjectWebServiceSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.gefersonholdorf.projectweb_servicespringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebServiceSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebServiceSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gefersonholdorf/projectweb_servicespringbootjpa/ProjectWebServiceSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gefersonholdorf.projectweb_servicespringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebServiceSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
