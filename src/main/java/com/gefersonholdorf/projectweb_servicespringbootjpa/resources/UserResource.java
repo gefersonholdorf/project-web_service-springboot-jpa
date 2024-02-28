d2a26e1f71de01a1b742d36d4e1650535986c553
Geferson Holdorf <gefersonholdorf88@gmail.com>
2024-02-28 20:34:14 -0300

    User entity and resource

---
+++ src/main/java/com/gefersonholdorf/projectweb_servicespringbootjpa/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.gefersonholdorf.projectweb_servicespringbootjpa.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gefersonholdorf.projectweb_servicespringbootjpa.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        return ResponseEntity.ok().body(null);
+    }
+}
