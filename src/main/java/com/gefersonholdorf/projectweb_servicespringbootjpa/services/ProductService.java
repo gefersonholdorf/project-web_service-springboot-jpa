630374c2aac75589306fd324f95de6c47ef079c1
Geferson Holdorf <gefersonholdorf88@gmail.com>
2024-02-29 17:29:49 -0300

    Product

---
+++ src/main/java/com/gefersonholdorf/projectweb_servicespringbootjpa/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.gefersonholdorf.projectweb_servicespringbootjpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gefersonholdorf.projectweb_servicespringbootjpa.entities.Product;
+import com.gefersonholdorf.projectweb_servicespringbootjpa.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }    
+
+    public Product findById(Long id) {
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
