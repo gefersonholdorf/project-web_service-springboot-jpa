07f486ee25b0c813aa29850cb79c93a30eb23dbf
Geferson Holdorf <gefersonholdorf88@gmail.com>
2024-02-29 20:33:05 -0300

    Exception Handling - findById

--- src/main/java/com/gefersonholdorf/projectweb_servicespringbootjpa/services/UserService.java
+++ src/main/java/com/gefersonholdorf/projectweb_servicespringbootjpa/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.gefersonholdorf.projectweb_servicespringbootjpa.entities.User;
 import com.gefersonholdorf.projectweb_servicespringbootjpa.repositories.UserRepository;
+import com.gefersonholdorf.projectweb_servicespringbootjpa.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
